e12c99f8ebd25b8a2f81751168862e2e954d47d5
idsiceman <reiter.oliver1990@gmail.com>
2015-01-07 18:19:59 +0100

    navigation drawer hinzugefügt

---
+++ app/src/main/java/com/apps/reo/inside_w/menu2_Fragment.java
@@ -0,0 +1,22 @@
+package com.apps.reo.inside_w;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by reo on 07.01.2015.
+ */
+public class menu2_Fragment extends Fragment {
+    View rootview;
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        rootview = inflater.inflate(R.layout.menu2, container, false);
+        return rootview;
+
+    }
+}
